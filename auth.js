0659130ffa3b4b5e271b83767627a6a14d8cd18c
ddtpunya <anthonyan4556@gmail.com>
2026-06-07 21:43:06 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,12 +6,11 @@ import {
 }
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
+import { setDoc, doc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const ALLOWED_EMAILS = [
-
     "anthonyan4556@gmail.com",
-
     "muhwahyuim261@gmail.com",
     "elmanjayahulu8@gmail.com"
 ];
@@ -28,11 +27,21 @@ loginBtn.addEventListener("click", async () => {
     }
 });
 
-onAuthStateChanged(auth,(user)=>{
+onAuthStateChanged(auth, async (user) => {
 
-    if(!user) return;
+    if (!user) return;
 
-    if(!ALLOWED_EMAILS.includes(user.email)){
+    // 🔥 SAVE USER KE FIRESTORE (INI YANG KAMU TAMBAH)
+    await setDoc(doc(db, "users", user.uid), {
+        uid: user.uid,
+        name: user.displayName || user.email.split("@")[0],
+        email: user.email,
+        photo: user.photoURL,
+        lastLogin: new Date()
+    });
+
+    // 🔒 CHECK ACCESS
+    if (!ALLOWED_EMAILS.includes(user.email)) {
 
         alert("Akses ditolak");
 
