b5119bc9b816b7da72271c815a33b99748c91f82
ddtpunya <anthonyan4556@gmail.com>
2026-06-07 07:04:43 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,14 +8,15 @@ import { getFirestore }
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-
-    apiKey: "AIzaSyB4VyaB_iWqnQvAdkk3rp_duVSIWTLROL8",
-
-    authDomain: "chat-app-cc947.firebaseapp.com",
-
-    projectId: "chat-app-cc947"
-
+  apiKey: "AIzaSyB4VyaB_iWqnQvAdkk3rp_duVSIWTLR0L8",
+  authDomain: "chat-app-cc947.firebaseapp.com",
+  projectId: "chat-app-cc947",
+  storageBucket: "chat-app-cc947.firebasestorage.app",
+  messagingSenderId: "666295111847",
+  appId: "1:666295111847:web:e23574eabaddf84eab8b82",
+  measurementId: "G-ECQPEE8RS"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
