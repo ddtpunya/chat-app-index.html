4aa13062a9302b5286d562bbb58b07a139246ff2
ddtpunya <anthonyan4556@gmail.com>
2026-06-07 07:15:52 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,6 @@
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
-import { getAuth }
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
-import { getFirestore }
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB4VyaB_iWqnQvAdkk3rp_duVSIWTLR0L8",
@@ -13,13 +8,10 @@ const firebaseConfig = {
   projectId: "chat-app-cc947",
   storageBucket: "chat-app-cc947.firebasestorage.app",
   messagingSenderId: "666295111847",
-  appId: "1:666295111847:web:e23574eabaddf84eab8b82",
-  measurementId: "G-ECQPEE8RS"
+  appId: "1:666295111847:web:e23574eabaddf84eab8b82"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
